b7d75aa72a70e4e162f03c0926869e26161df324
Adrian Jahraus <adrian.jahraus@proton.me>
2026-07-02 08:57:33 +0200

    Added to_lower get_date and get_user to stringconverter

--- include/utils/stringconverter.h
+++ include/utils/stringconverter.h
@@ -4,3 +4,6 @@
 #include <utility>
 
 std::pair<std::string, std::string> split_at(const std::string &input, const std::string &delimiter);
+std::string to_lower(const std::string &s);
+std::string get_date(const std::string &fmt);
+std::string get_user();
--- src/utils/stringconverter.cpp
+++ src/utils/stringconverter.cpp
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <utility>
+#include <chrono>
+
+using namespace std::chrono;
 
 std::pair<std::string, std::string> split_at(const std::string &input, const std::string &delimiter) {
   std::size_t pos = input.find(delimiter);
@@ -12,3 +15,21 @@ std::pair<std::string, std::string> split_at(const std::string &input, const std
       input.substr(pos + delimiter.length())
   };
 }
+
+std::string to_lower (const std::string& s) {
+  std::string out = s;
+  std::_Transform_vbool_aligned(out.begin(), out.end(), out.begin(), [](unsigned char c) { return std::tolower(c); });
+  return out;
+}
+
+std::string get_date (const std::string& fmt) {
+  auto now = system_clock::now();
+  auto today = floor<days>(now);
+  year_month_day ymd{today};
+  return std::vformat(fmt, std::make_format_args(ymd));
+}
+
+std::string get_user () {
+  return std::getenv("USERNAME");
+}
+
